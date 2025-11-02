4925c4ef3c0bb0c323dc7fe226c0da06b95add14
rayyansi9 <mrayyan@upei.ca>
2025-11-01 22:15:29 -0300

    Update features.py

---
+++ src/features.py
@@ -0,0 +1,4 @@
+# This file will be used for feature engineering or preprocessing steps.
+# There were no missing values in our dataset
+# Basic preprocessing (label creation and data splitting) is already handled
+# within data.py and train_baselines.py.
